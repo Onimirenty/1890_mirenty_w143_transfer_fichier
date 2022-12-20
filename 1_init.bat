7a7026def22d2d9797f8cc879fb4f1788a0ab8e7
onimirenty <mirentybg4@gmail.com>
2022-12-20 14:52:35 +0300

    suppression de plusieur fichier

--- 1_init.bat
+++
@@ -1,7 +0,0 @@
-javac -d . DirectoryRcr.java
-javac -d . DirectoryTxr.java
-javac -d . NeoFileChooser.java
-javac -d . Delete.java
-javac -d . ServerMain.java
-javac -d . ClientMain.java
-javac -d . Task.java
